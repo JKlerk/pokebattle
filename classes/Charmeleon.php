0ab52826c881929363e03096ebce68eb203fab01
JKlerk <jklerk@kiomota.com>
2019-03-26 15:07:00 +0100

    Fix var

--- classes/Charmeleon.php
+++ classes/Charmeleon.php
@@ -5,11 +5,13 @@ class Charmeleon extends Pokemon
 	public $energytype = "Fire";
 	public $hitpoints = '60';
 	public $health = '60';
-	public $weakness = ['Water', '2'];
-	public $resistance = ['Lightning', '10'];
+	public $weakness = ['Water', 2];
+	public $resistance = ['Lightning', 10];
 	public $attacks = [
 		'Head butt',
-		'Flare' 
+		10,
+		'Flare',
+		30
 	];
 
 	function __construct($name)
--- classes/Pikachu.php
+++ classes/Pikachu.php
@@ -5,11 +5,13 @@ class Pikachu extends Pokemon
 	public $energytype = "Lightning";
 	public $hitpoints = '60';
 	public $health = '60';
-	public $weakness = ['Fire', '1.5'];
-	public $resistance = ['Fighting', '20'];
+	public $weakness = ['Fire', 1.5];
+	public $resistance = ['Fighting', 20];
 	public $attacks = [
 		'Electric Ring',
-		'Pika Punch'
+		50,
+		'Pika Punch',
+		20
 	];
 
 	function __construct($name)
